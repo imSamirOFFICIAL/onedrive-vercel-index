ce5554e2ee62ad20f4c9398912f1553e2980721a
Samir <asamir192039@bscse.uiu.ac.bd>
2023-11-07 03:14:18 +0600

    Update FileListing.tsx

--- src/components/FileListing.tsx
+++ src/components/FileListing.tsx
@@ -21,6 +21,7 @@ import {
   traverseFolder,
 } from './MultiFileDownloader'
 
+import { layouts } from './SwitchLayout'
 import Loading, { LoadingIcon } from './Loading'
 import FourOhFour from './FourOhFour'
 import Auth from './Auth'
