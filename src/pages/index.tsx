5a3a18bdc4f8bef0703fff98e210d89b91e0ca91
Samir <asamir192039@bscse.uiu.ac.bd>
2023-11-07 03:06:47 +0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,6 @@ export default function Home() {
       <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">
         <Navbar />
         <div className="mx-auto w-full max-w-5xl py-4 sm:p-4">
-          <nav className="mb-4 flex items-center justify-between px-4 sm:px-0 sm:pl-1">
-            <Breadcrumb />
-            <SwitchLayout />
-          </nav>
           <FileListing />
         </div>
       </main>
