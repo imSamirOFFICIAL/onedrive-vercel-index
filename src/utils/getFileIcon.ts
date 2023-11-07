4a3b1c971b25384470ba102475da4d5d2e810cf3
Samir <asamir192039@bscse.uiu.ac.bd>
2023-11-07 15:54:36 +0600

    Update getFileIcon.ts

--- src/utils/getFileIcon.ts
+++ src/utils/getFileIcon.ts
@@ -7,7 +7,7 @@ const icons: { [key: string]: [IconPrefix, IconName] } = {
   powerpoint: ['far', 'file-powerpoint'],
   excel: ['far', 'file-excel'],
   audio: ['far', 'file-audio'],
-  video: ['far', 'file-video'],
+  video: ['fas', 'video'],
   archive: ['far', 'file-archive'],
   code: ['far', 'file-code'],
   text: ['far', 'file-alt'],
